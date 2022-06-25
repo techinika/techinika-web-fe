cde9f56b03c3a8cc87d85f5aef434e5c8d77cc06
songa1 <songachillethe1her@gmail.com>
2022-06-25 18:57:36 +0200

    Changed something on partners page

--- components/Partners.js
+++ components/Partners.js
@@ -16,14 +16,14 @@ export const Partners = () => {
     return (
         <>
             <div className="bg-gray-200" id="partners">
-                <div className="p-8 flex flex-col items-center mx-auto">
-                    <div className='w-full items-center md:w-10/12'>
+                <div className="w-10/12 p-8 flex flex-col items-center mx-auto">
+                    <div className='w-full items-center'>
                         <h1 className="text-mainBlue font-bold text-4xl my-4">Our Partners / Our clients</h1>
                         <p className='w-1/2'>
                         {"To deliver high quality service and best experience possible to our clients, we work with the the best companies and individuals who offer the best in their fields."}
                         </p>
                     </div>
-                    <div className="my-8 w-full md:w-10/12">
+                    <div className="my-8 w-full md:w-11/12">
                         <Slider {...settings}>
                             <div className="text-center">
                                 <Image src={techinikaLogo} width={100} height={100}/>
