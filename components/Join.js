0aafb3836e415e6c0e16db910415158269e23052
songa1 <songachillethe1her@gmail.com>
2023-05-27 19:39:07 +0200

    I have added new pages including study pages that link to two cards on the landing page

--- components/Join.js
+++ components/Join.js
@@ -8,7 +8,7 @@ export const JoinUs = () => {
                 <div className="flex-1 md:py-10 bg-blue-800">
                     <div className="md:w-10/12 ml-auto p-10 md:p-24">
                         <h2 className="title">Careers</h2>
-                        <p className="text">We are a team of highly motivated individuals working to create the country where Rwandans are able to use technology equipments available for them.</p>
+                        <p className="text-white py-4">We are a team of highly motivated individuals working to create the country where Rwandans are able to use technology equipments available for them.</p>
                         <div className="my-4">
                             <Link href="/careers">
                                 <a className="btn btn-yellow">Apply Now</a>
@@ -20,7 +20,7 @@ export const JoinUs = () => {
                 <div className="flex-1 md:p-10 bg-mainBlue">
                     <div className="md:w-10/12 p-10 md:p-24">
                         <h2 className="title">Collaborate</h2>
-                        <p className="text">We are a team of highly motivated individuals working to create the country where Rwandans are able to use technology equipments available for them.</p>
+                        <p className="text-white py-4">We are a team of highly motivated individuals working to create the country where Rwandans are able to use technology equipments available for them.</p>
                         <div className="my-4">
                             <Link href="/collaborate">
                                 <a className="btn btn-white">Join Us</a>
