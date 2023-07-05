239f954a7f2c182336007f505189086faf110851
Robert <robert@Emps-MacBook-Pro.local>
2023-07-06 01:57:31 +0200

    refactor: update code for  responsivenes,   add other team memberss

--- components/About/Team.js
+++ components/About/Team.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Francois from "../../public/team/Francois.jpg";
 import Achille from "../../public/team/songa achille c.jpg";
 import Ganza from "../../public/team/ganza.jpg";
+import g from "../../public/team/Gustave.jpg"
+import robert from "../../public/team/robert.png"
 
 export const Team = () => {
   return (
@@ -38,8 +40,27 @@ export const Team = () => {
             <p>Graphic Designer</p>
           </div>
         </div>
+        <div className="team-card">
+          <div className="team-image-div">
+            <Image src={g} className="team-image" layout="fill" />
+            <div className="image-placeholder"></div>
+          </div>
+          <div className="p-5 text-center">
+            <h3 className="team-name">Gustave</h3>
+            <p>Content creator</p>
+          </div>
+        </div>
+        <div className="team-card">
+          <div className="team-image-div">
+            <Image src={robert} className="team-image" layout="fill" />
+            <div className="image-placeholder"></div>
+          </div>
+          <div className="p-5 text-center">
+            <h3 className="team-name">Robert Niyitanga</h3>
+            <p>Web Developer</p>
+          </div>
+        </div>
       </div>
-      
     </div>
   );
 };
