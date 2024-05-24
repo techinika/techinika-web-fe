3374fcfac9ad83d91a128b8883d0785ac51c8b14
songa1 <songachillethe1her@gmail.com>
2024-05-24 18:27:41 +0200

    Added links to the product

--- components/Programmes.js
+++ components/Programmes.js
@@ -10,22 +10,33 @@ export const Programmes = () => {
             Products and Initiatives you can get from us:
           </p>
           <div className="grid grid-cols-4 justify-center items-stretch mx-auto mt-8 md:w-11/12 gap-4 sm:grid-cols-2 xs: grid-cols-1">
-            <div className="card">
-              <Image src="/learning.png" width={200} height={40} />
-            </div>
-            <div className="card">
-              <Image
-                src="/RWANDA TECHNOLOGY COMMUNITY LOGO.png"
-                width={200}
-                height={40}
-              />
-            </div>
-            <div className="card">
-              <Image src="/HP-Classeasy-Logo.png" width={200} height={40} />
-            </div>
-            <div className="card">
-              <Image src="/LeadXera-Logo.png" width={200} height={40} />
-            </div>
+            <Link href="https://techinika.com" target="_blank">
+              <div className="card">
+                <Image src="/learning.png" width={200} height={40} />
+              </div>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/showcase/rwanda-technology-community/"
+              target="_blank"
+            >
+              <div className="card">
+                <Image
+                  src="/RWANDA TECHNOLOGY COMMUNITY LOGO.png"
+                  width={200}
+                  height={40}
+                />
+              </div>
+            </Link>
+            <Link href="https://www.hpclasseasy.com/" target="_blank">
+              <div className="card">
+                <Image src="/HP-Classeasy-Logo.png" width={200} height={40} />
+              </div>
+            </Link>
+            <Link href="https://classera.com/leadxera/" target="_blank">
+              <div className="card">
+                <Image src="/LeadXera-Logo.png" width={200} height={40} />
+              </div>
+            </Link>
           </div>
         </div>
       </div>
