e05eb756f2f41e1507a106a1a0e2af8cd26e0220
songa1 <songachillethe1her@gmail.com>
2024-10-13 18:28:32 +0200

    I have added grid cols 2

--- components/Programmes.js
+++ components/Programmes.js
@@ -7,7 +7,7 @@ export const Programmes = () => {
       <div className="bg-gray-50 bg-no-repeat bg-cover py-4">
         <div className="md:w-10/12 xs:w-full mx-auto p-4 md:p-8">
           <p className="text-mainBlue font-bold text-center">Our Products:</p>
-          <div className="grid grid-cols-4 justify-center items-stretch mx-auto mt-8 md:w-11/12 gap-4 sm:grid-cols-2 xs: grid-cols-1">
+          <div className="grid grid-cols-2 justify-center items-stretch mx-auto mt-8 md:w-11/12 gap-4 sm:grid-cols-2 xs: grid-cols-1">
             <Link href="https://techinika.com" target="_blank">
               <div className="card">
                 <Image src="/learning.png" width={200} height={40} />
